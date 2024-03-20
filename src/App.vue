87cbaaa3b19b8f8a2dccf0596901d4c0ff938ec9
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-20 15:59:37 +0100

    add message from another component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <script>
+import BonusComponent from './components/BonusComponent.vue'
+
 export default {
   data() {
     return {
       message: 'Hello world'
     }
+  },
+  components: {
+    BonusComponent
   }
 }
 </script>
 
 <template>
   <h2>{{ message }}</h2>
+  <BonusComponent />
 </template>
 
 <style>
 h2 {
   color: palevioletred;
+  background-color: paleturquoise;
+  padding: 1rem;
+  width: fit-content;
 }
 </style>
---
+++ src/components/BonusComponent.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            msg: 'BonusComponent'
+        }
+    }
+}
+</script>
+
+<template>
+    <h3>message from {{ msg }}</h3>
+</template>
+
+<style>
+h3 {
+    color: teal;
+    background-color: paleturquoise;
+    padding: 1rem;
+    width: fit-content;
+}
+</style>
