4b1d62df3e8aec68750e369209224ac293116ac3
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-20 15:23:35 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello world'
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>{{ message }}</h2>
+</template>
+
+<style>
+h2 {
+  color: palevioletred;
+}
+</style>
